859dd31b3f92d26165055cc42ae109df387a5eae
tomo-create <wazatadayo@gmail.com>
2020-04-01 14:27:31 +0900

    スムーススクロールだけコピペでつけた。

---
+++ JS/script.js
@@ -0,0 +1,15 @@
+'use strict';
+
+// スムーススクロール
+$(function(){
+  // #で始まるリンクをクリックしたら実行されます
+  $('a[href^="#"]').click(function() {
+    // スクロールの速度
+    var speed = 400; // ミリ秒で記述
+    var href= $(this).attr("href");
+    var target = $(href == "#" || href == "" ? 'html' : href);
+    var position = target.offset().top;
+    $('body,html').animate({scrollTop:position}, speed, 'swing');
+    return false;
+  });
+});
